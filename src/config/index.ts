2a05e54a1022db76df955f3cdbdfbf3e664ac4bc
Ramen Kar <109908494+Kar1234Ramen@users.noreply.github.com>
2026-06-09 19:41:18 +0530

    config update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,18 @@
 import { config } from "dotenv";
 config();
 
-const { PORT, NODE_ENV } = process.env;
+const {
+  PORT,
+  NODE_ENV,
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  LASTFM_API_KEY,
+} = process.env;
 
 export const Config = {
   PORT,
   NODE_ENV,
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  LASTFM_API_KEY,
 };
